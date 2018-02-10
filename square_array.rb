78446151270f3fa88629baf1166cd55f730e82a2
Joseph Vint <josephvint@me.com>
2018-02-10 18:47:56 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,9 @@
-
 def square_array(array)
-  array.collect do |number|
-    number ** 2
+  index = 0
+  new_numbers = Array.new
+  array.each do |number|
+    new_numbers[index] = number ** 2
+    index += 1
   end
+  new_numbers
 end
